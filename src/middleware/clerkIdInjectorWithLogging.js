f35df32f4ad02e2ed419b83e05ecbb946c6df6bb
haiimal <abdillahakmal994@gmail.com>
2026-04-29 18:24:18 +0700

    fix : cors

--- src/middleware/clerkIdInjectorWithLogging.js
+++ src/middleware/clerkIdInjectorWithLogging.js
@@ -2,6 +2,10 @@
 import { verifyToken } from "@clerk/backend";
 
 export const clerkIdInjectorWithLogging = async (req, res, next) => {
+
+  // Skip validasi token untuk preflight request
+  if (req.method === "OPTIONS") return next();
+
   try {
     const authHeader = req.headers.authorization;
 
